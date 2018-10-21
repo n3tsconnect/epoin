c0ae66e14cb01cd490880463eccaf14e59a6c489
adrianardizza <adrian.ardizza@gmail.com>
2018-10-21 20:54:11 +0700

    s

--- halaman/admin/piket/pindai.php
+++ halaman/admin/piket/pindai.php
@@ -285,6 +285,7 @@
          function printfunction() {
          window.location.href="?halaman=piket&aksi=pindai&nis=<?php echo $pindai;?>";
          window.open('halaman/admin/piket/cetak.php?id=<?php echo $id_dispen; ?>&guru=<?php echo $id_guru;?>', 'mywindow', 'toolbar=0,scrollbars=1,statusbar=0,menubar=0,resizable=0,height=500,width=420');
+         
         }
          </script>
          <?php
