d42a8a4a8ac3f02e608b0c259ce3e90cb2144fb6
adrianardizza <adrian.ardizza@gmail.com>
2018-10-24 22:58:30 +0700

    test permit status #10

--- halaman/admin/piket/piket.php
+++ halaman/admin/piket/piket.php
@@ -68,47 +68,8 @@
 							<div class="card-header">
 									<strong class="card-title">Izin Terbaru</strong>
 							</div>
-							<div class="card-body card-block">
-							<table id="bootstrap-data-table2" class="table table-striped table-bordered">
-									<thead>
-										<tr>
-											<th>No</th>
-											<th>Siswa/i</th>
-											<th>Izin</th>
-											<th>Keterangan</th>
-											<th>Dari</th>
-											<th>Sampai</th>
-											<th>Tanggal</th>
-											<th>Petugas</th>
-											<th>Status</th>
-										</tr>
-									</thead>
-									<tbody>
-											<?php
-											$no   = 1;
-											$y  = $koneksi->query("SELECT * FROM tb_datadispen, tb_pengguna, tb_pelajar
-											WHERE tb_datadispen.id_pelajar = tb_pelajar.id_pelajar
-											AND tb_datadispen.id_guru = tb_pengguna.id_pengguna
-											AND CURTIME() > tb_datadispen.dari_kapan
-											AND CURTIME() < tb_datadispen.sampai_kapan
-											AND DATE(tb_datadispen.tgl_dibuat) = CURDATE()
-											ORDER by tgl_dibuat DESC LIMIT 10");
-											while ($pelanggaran = $y->fetch_assoc()){
-											?>
-										<tr>
-											<td><?php echo $no++;?></td>
-											<td><?php echo $pelanggaran['nama_pelajar']?></td>
-											<td><?php echo $pelanggaran['nama_dispen']?></td>
-											<td><?php echo $pelanggaran['deskripsi_dispen']?></td>
-											<td><?php echo date("H:i", strtotime($pelanggaran["dari_kapan"]))?>
-											<td><?php echo date("H:i", strtotime($pelanggaran["sampai_kapan"]))?></td>
-											<td><?php echo date("Y-m-d", strtotime($pelanggaran["tgl_dibuat"]))?></td>
-											<td><?php echo $pelanggaran['nama_pengguna']?></td>
-											<td><?php echo "Aktif" ?></td>
-										</tr>
-											<?php } ?>
-									</tbody>
-								</table>
+							<div class="card-body card-block" id="izin-dynamic">
+
 							</div>
 					</div>
 				</div>
