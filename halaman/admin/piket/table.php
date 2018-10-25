45b2ae67273a1ee5d2084abdc81313953d3e603b
adrianardizza <adrian.ardizza@gmail.com>
2018-10-25 10:42:49 +0700

    Added revoke button to table.php

--- halaman/admin/piket/table.php
+++ halaman/admin/piket/table.php
@@ -12,6 +12,7 @@
         <th>Petugas</th>
         <th>Waktu Tersisa</th>
         <th>Status</th>
+        <th>Control</th>
       </tr>
     </thead>
     <tbody>
@@ -36,7 +37,8 @@
         <td><?php echo date("Y-m-d", strtotime($pelanggaran["tgl_dibuat"]))?></td>
         <td><?php echo $pelanggaran['nama_pengguna']?></td>
         <td><?php echo gmdate("H:i:s", strtotime($pelanggaran["sampai_kapan"]) - time()) ?></td>
-        <td><?php echo "<p style='color:green'><b>Valid</b></p>" ?></td>
+        <td><?php echo "<p style='color:green'><b>Valid</b></p>" ?></td> 
+        <td><?php echo "<button class="btn btn-danger">Revoke</button> ?></td>
       </tr>
         <?php } ?>
     </tbody>
