d8ec8a5977d546fd93ee99aa1925542af51dc50e
adrianardizza <adrian.ardizza@gmail.com>
2018-10-25 11:21:07 +0700

    Update Table 17

--- halaman/admin/piket/table.php
+++ halaman/admin/piket/table.php
@@ -36,7 +36,7 @@
         <td><?php echo date("H:i", strtotime($pelanggaran["sampai_kapan"]))?></td>
         <td><?php echo date("Y-m-d", strtotime($pelanggaran["tgl_dibuat"]))?></td>
         <td><?php echo $pelanggaran['nama_pengguna']?></td>
-        <td><?php if (strtotime($pelanggaran["sampai_kapan"]) >= time()) { echo gmdate("H:i:s", strtotime($pelanggaran["sampai_kapan"]) - time()); } else { echo "<p style="color:red;"><b>HABIS</b></p> } ?></td>
+        <td><?php if (strtotime($pelanggaran["sampai_kapan"]) >= time()) { echo gmdate("H:i:s", strtotime($pelanggaran["sampai_kapan"]) - time()); } else { echo "<p style='color:red;'><b>HABIS</b></p>" } ?></td>
         <td><?php echo "<p style='color:green'><b>Valid</b></p>" ?></td> 
         <td><?php echo "<button class='btn btn-danger'>Revoke</button>" ?></td>
       </tr>
