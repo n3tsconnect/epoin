b80466cd48089c188138a8f35351a446e211bf01
Ibrahim Z. Faruqi <izfaruqi@gmail.com>
2018-10-29 11:48:27 +0700

    Tambah fungsi escape string untuk sql

--- tes/index.php
+++ tes/index.php
@@ -4,17 +4,13 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 
-<script type="text/javascript">
-$(function() {
-  $( "#nama_pelajar" ).autocomplete({
-    source: 'konek.php'
-  });
-});
-</script>
+
 </head>
 <body>
-<form method="post">
-<input type="teks" id="nama_pelajar"/>
-</form>
+<?php
+include('../konfigurasi/koneksi.php');
+$user = esc_trim($_GET['fds']);
+echo mysqli_real_escape_string($koneksi, $user);
+?>
 </body>
 </html>
