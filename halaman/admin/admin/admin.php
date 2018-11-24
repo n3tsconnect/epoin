c16f11be9bfdbc2e3ae6114d0d6cee4acb904d49
adrianardizza <adrian.ardizza@gmail.com>
2018-11-24 21:54:54 +0700

    Added new admin page with manual update function.

---
+++ halaman/admin/admin/admin.php
@@ -0,0 +1,33 @@
+<?php
+if (isset($_POST['update'])) {
+  $output = shell_exec ( "git pull" );
+}
+ ?>
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm-6">
+      <div class="card">
+        <div class="card-header">
+          <strong class="card-title">Update</strong>
+        </div>
+        <div class="card-body">
+          Update software E-Poin ke release terbaru dari repositori GitHub.
+          <form method="post">
+            <button name="update">Update</button>
+          </form>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-6">
+      <div class="card">
+        <div class="card-header">
+          <strong class="card-title">Output</strong>
+        </div>
+        <div class="card-body">
+          <div class="m-2">
+            <?php echo "<pre>$output</pre>"; ?>
+          </div>
+      </div>
+    </div>
+  </div>
