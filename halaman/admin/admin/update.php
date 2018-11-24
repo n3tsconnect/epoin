be2d5c37fb4492ac6638f25658690015ceb371a7
adrianardizza <adrian.ardizza@gmail.com>
2018-11-24 18:30:51 +0700

    Added payload script for update.php

--- halaman/admin/admin/update.php
+++ halaman/admin/admin/update.php
@@ -1,15 +1,5 @@
 <?php
-if (isset($_POST['updatebutton'])) {
+if (isset($_POST['payload'])) {
   $output = shell_exec ( "git pull" );
 }
  ?>
- <html>
- <body>
-   <form method="post">
-     <button name="updatebutton">Update</button>
-   </form>
-   <pre>
-     <?php echo $output; ?>
-   </pre>
- </body>
- </html>
