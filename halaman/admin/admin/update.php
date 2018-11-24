7552fd1cb166279a6222f43f02b366f54a59cef5
adrianardizza <adrian.ardizza@gmail.com>
2018-11-24 17:48:51 +0700

    Git Pull Script Test

---
+++ halaman/admin/admin/update.php
@@ -0,0 +1,14 @@
+<?php
+if (isset($_POST['updatebutton'])) {
+  $output = shell_exec ( /usr/bin/git pull );
+}
+ ?>
+ <html>
+ <body>
+   <form method="post">
+     <button name="updatebutton">Update</button>
+   </form>
+   <pre>
+     <?php echo $output; ?>
+   </pre>
+ </body>
