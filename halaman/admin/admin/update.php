3e0b120d1acf91dee58ab22ac0ffbb8fe1615282
adrianardizza <adrian.ardizza@gmail.com>
2018-11-24 18:05:29 +0700

    removed button from update.php

--- halaman/admin/admin/update.php
+++ halaman/admin/admin/update.php
@@ -1,13 +1,8 @@
 <?php
-if (isset($_POST['updatebutton'])) {
   $output = shell_exec ( "/usr/bin/git pull" );
-}
  ?>
  <html>
  <body>
-   <form method="post">
-     <button name="updatebutton">Update</button>
-   </form>
    <pre>
      <?php echo $output; ?>
    </pre>
