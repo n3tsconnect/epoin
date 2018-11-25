1880d2b10fc5303dbf24715562c031de4cedf301
Ibrahim Z. Faruqi <izfaruqi@gmail.com>
2018-11-25 11:20:30 +0700

    Logging pelajar dan disable sementara input foto pelajar

--- halaman/admin/pelajar/hapus.php
+++ halaman/admin/pelajar/hapus.php
@@ -2,5 +2,5 @@
     $id_pelajar = esc($_POST['id']);
     // Hapus id pelajarnya.
     $koneksi->query("DELETE FROM tb_pelajar WHERE id_pelajar = '$id_pelajar'");
-
+    action("PELAJAR_DELETE", array("idPelajar" => $id_pelajar));
     echo "DONE";
