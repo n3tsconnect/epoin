ba5ba974e729804cde1bef643a922ae3352526d8
Ibrahim Z. Faruqi <izfaruqi@gmail.com>
2018-11-25 12:47:06 +0700

    Hapus dan ubah pengguna logging

--- halaman/admin/guru/hapus.php
+++ halaman/admin/guru/hapus.php
@@ -2,6 +2,7 @@
     $id = esc($_GET['id']);
 // Hapus id pelajarnya.
     $koneksi->query("DELETE FROM tb_pengguna WHERE id_pengguna = '$id'");
+    action("PENGGUNA_DELETE", array("idPengguna" => $id));
 ?>
          <script type="text/javascript">
             alert("Data berhasil dihapus!");
