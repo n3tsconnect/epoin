3c7a3a83129f6013f9862a65cab55a6c02121bfa
adrianardizza <adrian.ardizza@gmail.com>
2018-11-27 14:20:58 +0700

    Update pelajar index

--- pelajar/index.php
+++ pelajar/index.php
@@ -2,7 +2,7 @@
 session_start();
 include ('../konfigurasi/koneksi.php');
 include ('tata_letak_pelajar/atas.php');
-if (!isset($_SESSION['id'])) {
+if ($_SESSION['id'] == "") {
     header("location: ../masuk.php");
 }
 $id_pelajar = $_SESSION['id'];
