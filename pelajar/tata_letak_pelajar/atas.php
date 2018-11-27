8482a1129c4968d67cc7b737129b79a3dde279dd
adrianardizza <adrian.ardizza@gmail.com>
2018-11-27 09:28:22 +0700

    Removed left side menu for pelajar page

--- pelajar/tata_letak_pelajar/atas.php
+++ pelajar/tata_letak_pelajar/atas.php
@@ -32,39 +32,7 @@
 <body>
 
 
-        <!-- Left Panel -->
-
-    <aside id="left-panel" class="left-panel">
-        <nav class="navbar navbar-expand-sm navbar-default">
-
-            <div class="navbar-header">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-menu" aria-controls="main-menu" aria-expanded="false" aria-label="Toggle navigation">
-                    <i class="fa fa-bars"></i>
-                </button>
-                <a class="navbar-brand" href="./"><img src="gambar/logo.png" alt="Logo"></a>
-                <a class="navbar-brand hidden" href="./"><img src="gambar/logo2.png" alt="Logo"></a>
-            </div>
-
-            <div id="main-menu" class="main-menu collapse navbar-collapse">
-                <ul class="nav navbar-nav">
-                    <li>
-                        <a href="index.php"> <i class="menu-icon fa fa-dashboard"></i>Beranda </a>
-                    </li>
-                    <h3 class="menu-title">Guru</h3><!-- /.menu-title -->
-                    <li><a href="?halaman=piket"> <i class="menu-icon fa fa-pencil"></i> Piket</a></li>
-                    <h3 class="menu-title">Admin</h3><!-- /.menu-title -->
-                    <li class="menu-item-has-children dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-user-o"></i>Pengguna</a>
-                        <ul class="sub-menu children dropdown-menu">
-                            <li><i class="fa fa-user"></i><a href="?halaman=guru">Guru</a></li>
-                            <li><i class="fa fa-users"></i><a href="?halaman=pelajar">Pelajar</a></li>
-                        </ul>
-                    </li>
-                    <li><a href="?halaman=pelanggaran"> <i class="menu-icon fa fa-warning"></i> Pelanggaran</a></li>
-                </ul>
-            </div><!-- /.navbar-collapse -->
-        </nav>
-    </aside><!-- /#left-panel -->
+        
 
     <!-- Left Panel -->
 
