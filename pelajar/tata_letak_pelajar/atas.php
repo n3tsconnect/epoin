10c69a31d31266a23f5fe8647be77c61aec1a027
adrianardizza <adrian.ardizza@gmail.com>
2018-11-27 14:48:50 +0700

    Placeholder fix 2

--- pelajar/tata_letak_pelajar/atas.php
+++ pelajar/tata_letak_pelajar/atas.php
@@ -76,7 +76,7 @@
                 <div class="col-sm-5">
                     <div class="user-area dropdown float-right">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <img class="user-avatar rounded-circle" style="width:35px; height:35px;" src="<?php if (file_exists('../gambar/profil/pelajar/'.$pelajar['foto_pelajar'].'')) { echo '../gambar/profil/pelajar/'.$pelajar['foto_pelajar'].''; } else {echo 'https://placekitten.com/g/200/200';} ?>"/>" alt="User Avatar">
+                            <img class="user-avatar rounded-circle" style="width:35px; height:35px;" src="<?php if (file_exists('../gambar/profil/pelajar/'.$pelajar['foto_pelajar'].'')) { echo '../gambar/profil/pelajar/'.$pelajar['foto_pelajar'].''; } else {echo 'https://placekitten.com/g/200/200';} ?>" alt="User Avatar">
                         </a>
 
                         <div class="user-menu dropdown-menu">
