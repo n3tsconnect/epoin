a4c7780392305392c0945794daa1a06e537f01ab
adrianardizza <adrian.ardizza@gmail.com>
2018-11-27 13:56:06 +0700

    Updated pelajar index to return to masuk.php after session expiration

--- pelajar/index.php
+++ pelajar/index.php
@@ -3,7 +3,7 @@ session_start();
 include ('../konfigurasi/koneksi.php');
 include ('tata_letak_pelajar/atas.php');
 if (!isset($_SESSION['id'])) {
-    header("location: masuk.php");
+    header("location: ../masuk.php");
 }
 $id_pelajar = $_SESSION['id'];
 $sql = $koneksi->query("SELECT * FROM tb_pelajar WHERE id_pelajar = $id_pelajar;");
