76208a0c466d28e216efd7f5ca4d49fb6d0c2ca8
Adrian Ardizza <adrian.ardizza@gmail.com>
2020-02-01 08:53:24 +0700

    Revert changes

--- halaman/admin/piket/pindai.php
+++ halaman/admin/piket/pindai.php
@@ -146,10 +146,6 @@
             </div>
             <div class="modal-body">
                 <form method="POST" id="form-pelanggaran">
-                    <?php 
-                    $token = getCSRFToken(); 
-                    echo '<input type="hidden" name="csrf_token" value="' . $token . '"/>';
-                    ?>
                     <input type="hidden" name="tambah-pelanggaran" value=1 />
                     <input type="hidden" name="id_pelajar" value=<?php echo $id ?> />
                     <div class="form-group">
