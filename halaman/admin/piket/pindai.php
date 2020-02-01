d30ae757540bd98443b37497fd276b06dfca72b4
Adrian Ardizza <adrian.ardizza@gmail.com>
2020-02-01 11:08:57 +0700

    test

--- halaman/admin/piket/pindai.php
+++ halaman/admin/piket/pindai.php
@@ -563,16 +563,14 @@
             var rowData = tabel_izin.row($(this).parents('tr')).data();
             window.open("halaman/admin/piket/cetak.php?id=" + rowData[0], '_blank');
         });
-
         jQuery("form").each(function() {
     var tokenElement = jQuery(document.createElement('input'));
     tokenElement.attr('type', 'hidden');
-    tokenElement.attr('name', 'token');
+    tokenElement.attr('name', 'csrf_token');
     tokenElement.val(<?= $token ?>);
     jQuery(this).append(tokenElement);
+  });
     });
-    });
-
     
 </script>
 
